1f093cf83c431e39568300aa7ade09b7ee5be0e0
dvogel <daniel.vogel@hallowelt.com>
2020-08-05 12:09:44 +0200

    Add booklevel hirachie to sections for PDF export - fix syntax
    [3.1.x]
    
    ERM20258
    
    Change-Id: I08b198577df017a4e504833a4e69cd8e64024dd5

--- includes/BookExportModulePDF.class.php
+++ includes/BookExportModulePDF.class.php
@@ -402,7 +402,7 @@ class BsBookExportModulePDF implements BsUniversalExportModule {
 
 		$iLevel = 0;
 		if ( isset( $aPage['number'] ) ) {
-			$number = trim( '.', $aPage['number'] );
+			$number = trim( $aPage['number'], '.' );
 			$iLevel = count( explode( '.', $number ) );
 		}
 
@@ -667,7 +667,7 @@ HERE
 	}
 
 	/**
-	 * @param \DOMDocument $node
+	 * @param \DOMDocument &$node
 	 * @param int $level
 	 */
 	private function addBooklevelToSection( &$node, $level ) {
