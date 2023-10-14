6ad9594acd82ddcd686946bb608cfd39607709e1
dvogel <daniel.vogel@hallowelt.com>
2023-10-14 17:23:51 +0200

    Add books overview export action
    [4.x]
    
    ERM33695
    
    Depends-on: I156da722a2029477e6a72156cebc8fc052b86f6c
    Change-Id: Ia4bc7b4cb47c4f227f102818f112b2a869296c75

---
+++ src/HookHandlers/BSBookshelf.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BlueSpice\UEModuleBookPDF\HookHandlers;
+
+use BlueSpice\Bookshelf\Hook\BSBookshelfBooksOverviewBeforeSetBookActions;
+use BlueSpice\UEModuleBookPDF\BooksOverviewActions\Export;
+use MediaWiki\SpecialPage\SpecialPageFactory;
+use Title;
+
+class BSBookshelf implements BSBookshelfBooksOverviewBeforeSetBookActions {
+
+	/**
+	 * @var SpecialPageFactory
+	 */
+	private $specialPageFactory = null;
+
+	/**
+	 * @param SpecialPageFactory $specialPageFactory
+	 */
+	public function __construct( SpecialPageFactory $specialPageFactory ) {
+		$this->specialPageFactory = $specialPageFactory;
+	}
+
+	/**
+	 * @param array &$actions
+	 * @param Title $book
+	 * @param string $displayTitle
+	 * @return void
+	 */
+	public function onBSBookshelfBooksOverviewBeforeSetBookActions(
+		array &$actions, Title $book, string $displayTitle
+	): void {
+		$actions['book_pdf'] = new Export( $book, $this->specialPageFactory, $displayTitle );
+	}
+
+}
