d9a2d2318f0d3ef6e8568efe3ab331e7ba11f52a
dvogel <daniel.vogel@hallowelt.com>
2023-10-14 17:23:51 +0200

    Add books overview export action
    [4.x]
    
    ERM33695
    
    Depends-on: I156da722a2029477e6a72156cebc8fc052b86f6c
    Change-Id: Ia4bc7b4cb47c4f227f102818f112b2a869296c75
    (cherry picked from commit 6ad9594acd82ddcd686946bb608cfd39607709e1)

---
+++ src/BooksOverviewActions/Export.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace BlueSpice\UEModuleBookPDF\BooksOverviewActions;
+
+use BlueSpice\Bookshelf\IBooksOverviewAction;
+use MediaWiki\SpecialPage\SpecialPageFactory;
+use Message;
+use Title;
+
+class Export implements IBooksOverviewAction {
+
+	/**
+	 * @var Title
+	 */
+	private $book = null;
+
+	/**
+	 * @var SpecialPageFactory
+	 */
+	private $specialPageFactory = null;
+
+	/**
+	 * @var string
+	 */
+	private $displayTitle = '';
+
+	/**
+	 * @param Title $book
+	 * @param SpecialPageFactory $specialPageFactory
+	 * @param string $displayTitle
+	 */
+	public function __construct( Title $book, SpecialPageFactory $specialPageFactory, string $displayTitle ) {
+		$this->book = $book;
+		$this->specialPageFactory = $specialPageFactory;
+		$this->displayTitle = $displayTitle;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName(): string {
+		return 'book_pdf';
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getPosition(): int {
+		return 20;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getClasses(): array {
+		return [];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getIconClasses(): array {
+		return [ 'bi-file-earmark-pdf-fill' ];
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return new Message( 'bs-uemodulebookpdf-books-overview-page-book-action-export-book-text' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		$titleText = $this->book->getPrefixedText();
+		if ( $this->displayTitle !== '' ) {
+			$titleText = $this->displayTitle;
+		}
+		return new Message(
+			'bs-uemodulebookpdf-books-overview-page-book-action-export-book-title',
+			[ $titleText ]
+		);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHref(): string {
+		$universalExport = $this->specialPageFactory->getPage( 'UniversalExport' );
+		$queryParams['ue[module]'] = 'bookpdf';
+
+		return $universalExport->getPageTitle( $this->book->getPrefixedText() )
+			->getLinkURL( array_merge( $queryParams, [] ) );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getRequiredPermission(): string {
+		return 'uemodulebookpdf-export';
+	}
+}
