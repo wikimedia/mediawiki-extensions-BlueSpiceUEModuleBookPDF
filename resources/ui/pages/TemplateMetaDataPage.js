895995eae75e0c4e48f3ffb523e32d173202561c
Mschlindwein <schlindwein@hallowelt.com>
2024-05-08 15:43:49 +0200

    Add metadata template
    ERM36908
    
    [4.5.x]
    
    Change-Id: I829b6e61f17b5ee324535095d6b0a65b28a12669
    (cherry picked from commit bb99654b341843b71fbca0e3b67bddfa4b829946)

---
+++ resources/ui/pages/TemplateMetaDataPage.js
@@ -0,0 +1,66 @@
+( function ( mw, $, bs ) {
+
+	bs.util.registerNamespace( 'bs.ue.ui.pages' );
+
+	bs.ue.ui.pages.TemplateMeta = function( name, config ) {
+		config = config || {};
+		bs.ue.ui.pages.TemplateMeta.super.call( this, name, config );
+	};
+
+	OO.inheritClass( bs.ue.ui.pages.TemplateMeta, bs.bookshelf.ui.pages.MetaDataPage );
+
+	bs.ue.ui.pages.TemplateMeta.prototype.getOutlineLabel = function () {
+		return mw.message( 'bs-uemodulebookpdf-template' ).text();
+	};
+
+	bs.ue.ui.pages.TemplateMeta.prototype.setup = function () {
+		var values = require( './templatedata.json' );
+		var availableTemplates = values.availableTemplates;
+		var defaultTemplate = values.defaultTemplate;
+		var options = [];
+		if ( availableTemplates.length > 1 ) {
+			for ( var entry in availableTemplates ) {
+				var item = new OO.ui.MenuOptionWidget( {
+					data: encodeURI( availableTemplates[ entry ] ),
+					label: availableTemplates[ entry ]
+				} );
+				options.push( item );
+			}
+		} else {
+			var item = new OO.ui.MenuOptionWidget( {
+				data: encodeURI( defaultTemplate ),
+				label: defaultTemplate
+			} );
+			options.push( item );
+		}
+
+		this.inputWidget = new OO.ui.DropdownWidget( {
+			menu: {
+				items: options
+			},
+			$overlay: true
+		} );
+
+		if ( availableTemplates.length === 1 ) {
+			this.inputWidget.getMenu().selectItemByData( options[0].data );
+			this.inputWidget.setDisabled( true );
+		}
+
+		var fieldLayout = new OO.ui.FieldLayout( this.inputWidget, {
+			align: 'top',
+			label: mw.message( 'bs-uemodulebookpdf-template' ).text()
+		} );
+
+		this.$element.append( fieldLayout.$element );
+	};
+
+	bs.ue.ui.pages.TemplateMeta.prototype.getValue = function () {
+		return this.inputWidget.getValue();
+	};
+
+	bs.ue.ui.pages.TemplateMeta.prototype.setValue = function ( value ) {
+		this.inputWidget.setValue( value );
+	};
+
+
+} )( mediaWiki, jQuery, blueSpice );
---
+++ src/ClientConfig.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace BlueSpice\UEModuleBookPDF;
+
+use MediaWiki\MediaWikiServices;
+
+class ClientConfig {
+
+	/**
+	 *
+	 * @return array
+	 */
+	public static function getBookTemplates() {
+		$services = MediaWikiServices::getInstance();
+		$config = $services->getConfigFactory()->makeConfig( 'bsg' );
+
+		$defaultTemplate = $config->get( 'UEModuleBookPDFDefaultTemplate' );
+		$defaultTemplatePath = $config->get( 'UEModuleBookPDFTemplatePath' );
+
+		$availableTemplates = [];
+		$dir = opendir( $defaultTemplatePath );
+		if ( $dir ) {
+			$subDir = readdir( $dir );
+			while ( $subDir !== false ) {
+				if ( in_array( $subDir, [ '.', '..', 'common' ] ) ) {
+					$subDir = readdir( $dir );
+					continue;
+				}
+
+				if ( !is_dir( "{$defaultTemplatePath}/{$subDir}" ) ) {
+					$subDir = readdir( $dir );
+					continue;
+				}
+
+				if ( file_exists( "{$defaultTemplatePath}/{$subDir}/template.php" ) ) {
+					$availableTemplates[] = $subDir;
+				}
+
+				$subDir = readdir( $dir );
+			}
+		}
+
+		if ( empty( $availableTemplates ) ) {
+			$defaultTemplate = '';
+		} else {
+			if ( !in_array( $defaultTemplate, $availableTemplates ) ) {
+				$defaultTemplate = $availableTemplates[0];
+			}
+		}
+		return [
+			'defaultTemplate' => $defaultTemplate,
+			'availableTemplates' => $availableTemplates
+		];
+	}
+}
