f2081750cdc5e561bb844a6de41818b6493fd3a2
Mschlindwein <schlindwein@hallowelt.com>
2024-05-08 15:43:49 +0200

    Add metadata template
    ERM36908
    
    [4.5.x]
    
    Change-Id: I829b6e61f17b5ee324535095d6b0a65b28a12669
    (cherry picked from commit bb99654b341843b71fbca0e3b67bddfa4b829946)

---
+++ src/MetaData/Template.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BlueSpice\UEModuleBookPDF\MetaData;
+
+use BlueSpice\Bookshelf\IMetaDataDescription;
+use Message;
+
+class Template implements IMetaDataDescription {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getKey(): string {
+		return 'template';
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getName(): Message {
+		return Message::newFromKey( 'bs-uemodulebookpdf-template' );
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getRLModules(): array {
+		return [ 'ext.bluespice.ueModuleBookPDF.meta.template' ];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getJSClassname(): string {
+		return 'bs.ue.ui.pages.TemplateMeta';
+	}
+
+}
